89c40e4c76b854ff9cb08af66c1c3883632ceec7
bacila1990 <1smeh.ru@mail.ru>
2019-10-09 12:40:19 +0300

    task complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let row = str;
+  for (let i = 0; i < bracketsConfig.length;) {
+    const value = bracketsConfig[i].join('');
+    if (row.includes(value)) {
+      row = row.replace(value, '');
+      i = 0;
+    } else {
+      i += 1;
+    }
+  }
+  if (row.length === 0) {
+    return true;
+  }
+  return false;
+};
